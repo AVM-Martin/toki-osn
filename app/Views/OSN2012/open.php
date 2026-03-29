38b96d41a26f46d87a5fe6c3a1e5c5782023322c
AVM.Martin <avmm.martin+github@gmail.com>
2026-03-30 03:26:34 +0700

    feat(OSN2012): open osn data
    Refs: https://toki.id/2012/osn-informatika-terbuka-2012/
    Refs: https://web.archive.org/web/20121004015337/http://tokilearning.org/public/OSN%20Terbuka%202012/Info_OSN_2012_terbuka.pdf

---
+++ app/Views/OSN2012/open.php
@@ -0,0 +1,10 @@
+<div class="bp3-card">
+    <h3>
+        <a href="https://toki.id/2012/osn-informatika-terbuka-2012/" target="blank">OSN Informatika Terbuka 2012</a>
+    </h3>
+
+    <p>OSN Informatika Terbuka 2012 (untuk selanjutnya disebut OSN Terbuka) adalah olimpiade yang berbentuk kontes pemrograman yang diadakan bersamaan dengan OSN 2012 bidang informatika yang diadakan di Jakarta (untuk selanjutnya disebut OSN 2012).</p>
+    <p>Tujuan diadakannya OSN Terbuka ini adalah untuk memberikan kesempatan kepada peserta yang karena satu dan lain hal tidak diundang ke OSN 2012. OSN Terbuka dilaksanakan dengan menggunakan prosedur dan aturan yang serupa dengan OSN 2012. OSN Terbuka dilaksanakan secara online. Kontes dapat diikuti dari tempat masing-masing peserta. OSN Terbuka dapat diikuti oleh siapa pun yang telah memenuhi prosedur pendaftaran.</p>
+    <p>Kontes OSN Informatika Terbuka, sama seperti kontes OSN Informatika 2012, terbagi menjadi dua hari, yaitu kontes pada tanggal 4 September 2012 dan 5 September 2012.</p>
+    <p>Untuk informasi lebih lanjut dapat dilihat pada <a href="#">dokumen panduan OSN Informatika 2012</a></p>
+</div>

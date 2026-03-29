432e4bdcab5a013394b3576f8504f5ba0d9c24ff
AVM.Martin <avmm.martin+github@gmail.com>
2026-03-30 03:57:51 +0700

    feat(OSN2010): open osn data
    Refs: https://toki.id/2010/osn-informatika-terbuka-2010/
    Refs: https://toki.id/2010/osn-2010-informasi-kompetisi/
    Refs: https://toki.id/2010/osn-informatika-terbuka-2010-hasil/

---
+++ app/Views/OSN2010/open.php
@@ -0,0 +1,33 @@
+<div class="bp3-card">
+    <h3>
+        <a href="https://toki.id/2010/osn-informatika-terbuka-2010/" target="blank">OSN Informatika Terbuka 2010</a>
+    </h3>
+
+    <p>Dengan ini, diumumkan bahwa pada tahun ini akan diadakan OSN Informatika Terbuka 2010 untuk memberikan kesempatan kepada peserta yang karena satu dan lain hal tidak diundang ke OSN 2010 yang diadakan di Medan, Sumatera Utara.</p>
+    <p>Perhatikan berkas prosedur peserta yang terlampir (prosedur tersebut bersifat fleksibel, yang berarti bahwa mungkin ada perubahan yang diinformasikan kemudian).</p>
+    <p>Hal yang sangat penting dari berkas prosedur tersebut antara lain adalah:</p>
+    <ol>
+        <li>Soal, pembobotan nilai, penentuan pemenang, dan jadwal OSN Terbuka menyerupai OSN 2010 di Medan.</li>
+        <li>Terdapat 2 macam peserta, online atau onsite. Onsite hanya dapat diikuti siswa SMP/sederajat dan SMA/sederajat.</li>
+        <li>Terdapat 8 pilihan lokasi keikutsertaan untuk peserta onsite: UI, ITB, UGM, ITS, USU, UNAND, IPB, dan SMAN 9 Manado.</li>
+        <li>Registrasi dilakukan melalui email <a href="#">open.osn@gmail.com</a> (bukan menghubungi langsung lokasi pelaksanaan ataupun kontak lainnya) <strong>dan karoldanutama@gmail.com</strong> selambat-lambatnya 1 Agustus 2010 (first come first serve) dengan menyertakan informasi Nama Lengkap, Tempat Tanggal Lahir, Alamat Rumah, Asal Sekolah, Alamat Sekolah, dan Pilihan Lokasi Pelaksanaan.</li>
+    </ol>
+    <p>Demikian pengumuman ini. Selamat berkompetisi, jadilah yang terbaik.</p>
+    <p>Pertanyaan seputar OSN Terbuka ini dapat juga dikirimkan melalui email ke <a href="#">open.osn@gmail.com</a>.</p>
+    <p><a href="#">Prosedur_OSN_Informatika_Terbuka_2010_v-4.3_Peserta</a></p>
+
+    <hr />
+
+    <h3 class="post-title">
+        <a href="https://toki.id/2010/osn-2010-informasi-kompetisi/" target="blank">OSN 2010 – Informasi Kompetisi</a>
+    </h3>
+
+    <p><strong>Kompetisi</strong> OSN 2010 Bidang Informatika dibagi ke dalam tiga sesi yang harus diikuti oleh setiap peserta yakni sesi 1 sampai sesi 3.</p>
+    <ul>
+        <li><strong>Sesi 1</strong> berisi persoalan teori dalam bentuk isian singkat yang bersifat algoritmis dan analitis. Sesi 1 dapat dikerjakan dengan atau tanpa komputer. Peserta dapat mengerjakan semua soal sesi 1 yang berjumlah 60 dalam waktu 210 menit. Terdapat 60 menit yang tumpang tindih dengan sesi 2 (di penghujung sesi 1). Pada rentang waktu tersebut, peserta bebas memilih untuk mengerjakan soal sesi 1, sesi 2, atau keduanya sekaligus.</li>
+        <li><strong>Sesi 2</strong> berisi soal-soal pemrograman sederhana yang menguji kemampuan peserta dalam menulis kode program pada bahasa Pascal atau C/C++. Peserta dapat mengerjakan semua soal sesi 2 yang berjumlah 8 dalam waktu 150 menit. Terdapat 60 menit tumpang tindih dengan sesi 1 (di awal sesi 2). Untuk 90 menit pertama di sesi 2, penonton di luar kompetisi dapat melihat perolehan nilai semua peserta di tempat yang akan ditentukan.</li>
+        <li><strong>Sesi 3</strong> berisi soal-soal pemrograman problem solving yang menguji kemampuan peserta dalam menyelesaikan persoalan algoritmis melalui kode program. Peserta dapat mengerjakan semua soal sesi 3 yang berjumlah 5 dalam waktu 300 menit. Untuk 210 menit pertama di sesi 3, penonton di luar kompetisi dapat melihat perolehan nilai semua peserta di tempat yang akan ditentukan.</li>
+    </ul>
+    <p><strong>Bobot nilai</strong> setiap sesi adalah berbeda. Sesi 1 memiliki bobot 30%, sedangkan sesi 2 dan sesi 3 masing-masing 20% dan 50%. Perolehan nilai akhir untuk akan dipublikasikan setelah pembagian medali.</p>
+    <p>OSN Bidang Informatika dapat diikuti di beberapa lokasi bagi peserta yang tidak berhasil lulus ke OSN 2010. Kegiatan tersebut dinamakan <strong>OSN Informatika Terbuka 2010</strong>. Lokasi yang dimaksud adalah UI, ITB, UGM, ITS, USU, dan UNAND. Untuk OSN Terbuka, peserta hanya mengikuti sesi latihan dan sesi kompetisi. Ini adalah salah satu upaya untuk menjaring bakat-bakat luar biasa yang ternyata kurang beruntung, tidak terjaring melalui jalur resmi OSN.</p>
+</div>

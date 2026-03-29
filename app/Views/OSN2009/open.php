8840e4d83742a4622b9020dc386d0ff1595d3e3c
AVM.Martin <avmm.martin+github@gmail.com>
2026-03-30 03:22:08 +0700

    feat(OSN2009): open osn data
    Refs: https://toki.id/2009/open-osn-2009/
    Refs: https://toki.id/2009/238/
    Refs: https://toki.id/tokifiles/2011/06/Publish_Version_Official.pdf
    Refs: https://toki.id/tokifiles/2011/06/Publish_Version_Open_Onsite.pdf
    Refs: https://toki.id/tokifiles/2011/06/Publish_Version_Online.pdf

---
+++ app/Views/OSN2009/open.php
@@ -0,0 +1,53 @@
+<div class="bp3-card">
+    <h3>
+        <a href="https://toki.id/2009/open-osn-2009/" target="blank">Open OSN 2009</a>
+    </h3>
+
+    <p>Dengan ini diinformasikan bahwa OSN 2009 akan dapat diikuti oleh siapapun yang berminat, baik secara <strong>onsite</strong> (datang langsung ke Fasilkom UI) atau secara <strong>online</strong>. Selanjutnya OSN 2009 terbuka untuk peserta tidak resmi ini akan disebut <strong>Open OSN 2009.</strong></p>
+    <p>Berikut ini informasi mengenai <strong>Open OSN 2009 onsite</strong>:</p>
+
+    <ul>
+        <li>Open OSN 2009 onsite akan diadakan di Fakultas Ilmu Komputer Universitas Indonesia Gedung A Lantai 1 Ruang 1103.</li>
+        <li>Open OSN 2009 onsite akan diadakan pada tanggal 5 dan 6 Agustus 2009, pukul 09.00 – 14.00 WIB.</li>
+        <li>Open OSN 2009 onsite boleh diikuti siapapun, bukan hanya pelajar SMA. Tapi karena tempat yang sangat terbatas (yakni lebih kurang 60 komputer), peserta OSP 2009 akan mendapatkan prioritas yang lebih tinggi, selebihnya akan digunakan sistem “first come first serve”.</li>
+        <li>Peserta Open OSN 2009 onsite akan mengikuti semua sesi pertandingan dengan soal yang sama yang diberikan untuk OSN 2009.
+            <ul>
+                <li>Hari Pertama : Sesi 1 (Pilihan Ganda dan Isian Singkat) dan Sesi 2 (Pemrograman Sederhana)</li>
+                <li>Hari Kedua : Sesi 3 (Pemrograman Kompleks)</li>
+            </ul>
+        </li>
+        <li>Pendaftaran dapat dilakukan melalui <a href="#">http://fpc.cs.ui.ac.id/openosn/2009/index.php</a>, paling lambat 4 Agustus 2009 pukul 20.00 WIB.</li>
+        <li>Mohon membawa bukti identitas (KTP atau Kartu Pelajar) untuk melakukan daftar ulang sebelum Open OSN 2009 onsite dimulai.</li>
+        <li>Panitia tidak bertanggung jawab atas akomodasi maupun tranportasi peminat Open OSN 2009 onsite.</li>
+        <li>Disediakan hadiah menarik dan sertifikat dari pembina TOKI bagi peserta yang masih duduk di kelas XI SMA atau lebih rendah yang berhasil mendapatkan nilai lebih tinggi atau sama dengan peraih medali perak OSN 2009.</li>
+        <li>Hasil Open OSN 2009 onsite akan dipublikasikan bersamaan atau setelah pengumuman hasil resmi OSN 2009.</li>
+    </ul>
+
+    <hr />
+
+    <p>Berikut ini informasi mengenai <strong>Open OSN 2009 online</strong>:</p>
+    <ul>
+        <li>Open OSN 2009 online akan diadakan melalui TOKI Learning Center 2 (<a href="#">http://lc.toki.if.itb.ac.id/lc/index.php</a>).</li>
+        <li>Open OSN 2009 online akan diadakan pada tanggal 5 dan 6 Agustus 2009, pukul 09.00 – 14.00 WIB.</li>
+        <li>Open OSN 2009 online boleh diikuti siapapun, bukan hanya pelajar SMA. Berbeda dengan onsite, tempat bisa dikatakan tidak terbatas untuk kompetisi secara online ini.</li>
+        <li>Peserta Open OSN 2009 online hanya akan mengikuti Sesi 2 (Pemrograman Sederhana) pada hari pertama dan Sesi 3 (Pemrograman Kompleks) pada hari kedua. Sesi 1 (Pilihan Ganda dan Isian Singkat) tidak diberikan pada Open OSN 2009 online.</li>
+        <li>Pendaftaran dapat dilakukan melalui email yang dikirimkan ke <a href="#">open.osn@gmail.com</a> dengan judul Pendaftaran Open OSN 2009 online, paling lambat 6 Agustus 2009 pukul 12.00 WIB. Email yang bersangkutan harus berisi dan hanya berisi data-data sebagai berikut dengan urutan yang benar:
+            <ul>
+                <li>Nama Lengkap:</li>
+                <li>Alamat Rumah:</li>
+                <li>Alamat Email:</li>
+                <li>Nomor Telepon</li>
+                <li>Nomor Ponsel:</li>
+                <li>Sekolah/Institusi:</li>
+            </ul>
+        </li>
+        <li>Email yang tidak sesuai dengan syarat di atas tidak akan mendapatkan email balasan. Email yang sesuai dengan syarat di atas akan mendapatkan email balasan berisi Username dan Password.</li>
+        <li>Di sela-sela waktu, peserta Open OSN 2009 online juga dapat mencoba mengerjakan soal-soal Chapter 6 PJJ Pra OSN 2009 yang kebanyakan merupakan soal OSN tahun-tahun sebelumnya.</li>
+        <li>Panitia tidak menjanjikan hadiah apapun (setidaknya untuk saat ini) untuk Open OSN 2009 online.</li>
+        <li>Hasil Open OSN 2009 online akan dipublikasikan bersamaan atau setelah pengumuman hasil resmi OSN 2009.</li>
+    </ul>
+
+    <hr />
+
+    <p>Untuk pertanyaan lebih lanjut seputar Open OSN 2009, silakan kirim email ke <strong>open.osn@gmail.com</strong>.</p>
+</div>

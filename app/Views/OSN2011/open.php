317132c367169629d5df62ae3b0be6620d9f33f2
AVM.Martin <avmm.martin+github@gmail.com>
2026-03-30 03:57:16 +0700

    feat(OSN2011): open osn data
    Refs: https://toki.id/2011/olimpiade-sains-nasional-informatika-terbuka-2011/
    Refs: https://toki.id/2011/olimpiade-sains-nasional-informatika-terbuka-2011-hasil/
    Refs: https://web.archive.org/web/20111116061843/http://tokilearning.org/public/OSN%20Terbuka%202011/OSN%20Informatika%20Terbuka%202011%20-%20Tabel%20Nilai.pdf
    Refs: https://web.archive.org/web/20111116190650/http://tokilearning.org/public/OSN%20Terbuka%202011/OSN%20Informatika%20Terbuka%202011%20-%20Peringkat%20Peserta%20Onsite.pdf

---
+++ app/Views/OSN2011/open.php
@@ -0,0 +1,37 @@
+<div class="bp3-card">
+    <h3>
+        <a href="https://toki.id/2011/olimpiade-sains-nasional-informatika-terbuka-2011/" target="blank">Olimpiade Sains Nasional Informatika Terbuka 2011</a>
+    </h3>
+
+    <img src="/images/openosn2015.png" width="250">
+
+    <p><strong>Olimpiade Sains Nasional Informatika Terbuka 2011</strong> (untuk selanjutnya disebut OSN Terbuka) adalah olimpiade yang berbentuk kompetisi pemrograman yang diadakan bersamaan dengan OSN 2011 bidang informatika yang diadakan di Manado, Sulawesi Utara.</p>
+    <p>Tujuan diadakannya OSN Terbuka ini adalah untuk memberikan kesempatan kepada peserta yang karena satu dan lain hal tidak diundang ke OSN 2011 di Manado. OSN Terbuka dilaksanakan dengan menggunakan <strong>prosedur dan aturan yang serupa </strong>dengan OSN Informatika 2011.</p>
+    <p>OSN Terbuka diadakan tanggal <strong>12 s.d. 14 September 2011</strong> secara <strong>onsite </strong>dan <strong>online</strong>. Kompetisi onsite dapat diikuti di beberapa perguruan tinggi yang sudah bekerja sama dengan panitia OSN Terbuka, sedangkan kompetisi online dapat diikuti dari tempat masing-masing peserta. OSN Terbuka dapat diikuti oleh siapa pun yang memenuhi syarat sebagai peserta.</p>
+    <p>Setiap calon peserta <strong>wajib mengunduh dan membaca memahami dokumen Pedoman OSN Informatika Terbuka 2011 terlampir</strong>. Pastikan Anda memahami poin-poin berikut:</p>
+
+    <ol>
+        <li>Soal, pembobotan nilai, dan jadwal OSN Terbuka menyerupai OSN Informatika 2011 di Manado</li>
+        <li>Terdapat dua macam peserta, <strong>online atau onsite</strong></li>
+        <li>Terdapat <strong>enam site OSN Terbuka onsite</strong>: UI, ITB, UGM, ITS, IPB, dan UNAND.</li>
+        <li>Prosedur <strong>pendaftaran dilakukan di www.tokilearning.org</strong> pada <strong>menu ‘Kontes’ OSN Informatika Terbuka 2011</strong> dan <strong>jangan lupa untuk memilih site yang Anda inginkan di menu <a href="#">‘Sunting Profil’</a></strong> (pendaftaran baru dianggap valid jika kolom ‘Site’ tidak kosong). Lengkapi juga bagian <strong>Informasi Umum</strong> untuk mempermudah administrasi lomba.</li>
+    </ol>
+
+    <p>Pertanyaan mengenai OSN Terbuka dapat ditanyakan melalui email ke <em>open.osn@gmail.com</em>.</p>
+    <p>Demikian pengumuman ini, selamat berkompetisi!</p>
+    <p>
+        <em>Sukses TOKI !<br />
+        Go Get Golds !<br />
+        Bandung, 3 September 2011<br />
+        Tim Olimpiade Komputer Indonesia<br />
+        Koordinator Pusat OSN Informatika Terbuka 2011</em><br />
+        <em>Antonia Riani Kalisa</em><br />
+    </p>
+
+    <hr />
+
+    <p><strong>Lampiran : </strong></p>
+    <ul>
+        <li><a href="#">Pedoman OSN Informatika Terbuka 2011</a></li>
+    </ul>
+</div>

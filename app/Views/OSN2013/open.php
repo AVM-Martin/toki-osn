8255fb6f42b41a78ad1be7dac2aaec37fe276412
AVM.Martin <avmm.martin+github@gmail.com>
2026-03-29 15:28:32 +0700

    feat(OSN2013): open osn data
    Refs: https://toki.id/2013/osn-informatika-terbuka-2013-jadwal-pelaksanaan/
    Refs: https://toki.id/2013/osn-informatika-terbuka-2013/
    Refs: https://toki.id/2013/osn-informatika-terbuka-2013-hasil/

---
+++ app/Views/OSN2013/open.php
@@ -0,0 +1,46 @@
+<div class="bp3-card">
+    <h3>
+        <a href="https://toki.id/2013/osn-informatika-terbuka-2013/" target="blank">OSN Informatika Terbuka 2013</a>
+    </h3>
+
+    <p><strong>OSN Informatika Terbuka 2013</strong> (untuk selanjutnya disebut OSN Terbuka) adalah olimpiade yang berbentuk kontes pemrograman yang diadakan bersamaan dengan OSN 2013 bidang informatika yang diadakan di Bandung, Jawa Barat (untuk selanjutnya disebut OSN 2013).</p>
+
+    <img src="/images/openosn2015.png" width="250">
+
+    <p>Tujuan diadakannya OSN Terbuka ini adalah untuk memberikan kesempatan kepada peserta yang karena satu dan lain hal tidak diundang ke OSN 2013. OSN Terbuka dilaksanakan dengan menggunakan prosedur dan aturan yang serupa dengan OSN 2013. OSN Terbuka dilaksanakan secara online. Kontes dapat diikuti dari tempat masing-masing peserta. OSN Terbuka dapat diikuti oleh siapa pun yang telah memenuhi prosedur pendaftaran.</p>
+    <p>OSN Terbuka akan dibagi menjadi dua kategori, <strong>kategori SMA</strong> dan <strong>kategori Umum</strong>. Pemisahan ini bertujuan untuk mengetahui kemampuan siswa/i SMA/ sederajat yang berkompetisi di OSN Terbuka dan mendapatkan penilaian yang bersih (terpisah dari kategori yang sudah lulus SMA). Untuk Anda yang sudah tidak duduk di bangku SMA, bisa mengikuti OSN Terbuka kategori Umum.</p>
+
+    <hr />
+
+    <h3><strong>Prosedur Pendaftaran Kategori Umum</strong></h3>
+    <p>Prosedur <strong>pendaftaran dilakukan di <a href="#">TOKI Learning Center</a></strong>. Bagi yang belum memiliki akun, silahkan membuat akun baru terlebih dahulu di bagian kanan antarmuka <a href="#">TOKI Learning Center</a>. Setelah login, pilih menu <strong>Kontes</strong> di sebelah kiri dan mendaftarkan diri pada kontes <strong>“OSN Informatika Terbuka 2013 – Kategori Umum”</strong>. Sebelum melakukan pendaftaran, calon peserta <strong>diwajibkan</strong> mengisi informasi mengenai data diri pada bagian <strong>Sunting Profil</strong>. Pendaftaran calon peserta dianggap valid, jika telah <strong>melengkapi Informasi Umum</strong> dan pada kolom ‘Site’ memilih <strong>Online</strong>. Data diri yang kurang jelas dapat menghambat proses pendaftaran, untuk itu isilah data diri dengan jelas.</p>
+
+    <hr />
+
+    <h3><strong>Prosedur Pendaftaran Kategori SMA</strong></h3>
+    <p>Pendaftaran dilakukan di <a href="#">form berikut</a>. Data yang dimasukkan harus jelas dan bisa dipertanggung jawabkan. Para calon peserta yang telah mengisi form tersebut, akan mendapatkan email konfirmasi mengenai status pendaftaran.</p>
+    <p><strong>Diharapkan bagi peserta untuk tidak mendaftarkan diri pada kedua kontes secara bersamaan untuk mempermudah administrasi pengurusan data peserta.</strong></p>
+
+    <hr />
+
+    <p>Soal, pembobotan nilai serta jadwal OSN Informatika Terbuka 2013 akan menyerupai OSN Informatika 2013 di Bandung (untuk itu harap terlebih dahulu membaca <a href="#">panduan OSN Informatika 2013</a>). OSN Informatika Terbuka akan dilaksanakan secara serentak dengan OSN Informatika 2013 dengan jadwal sebagai berikut:</p>
+    <ul>
+        <li>Batas Pendaftaran : 2 September 2013, pukul 23:59 WIB (GMT +7)</li>
+        <li>Sesi Latihan (Hari 0) : 3 September 2013</li>
+        <li>Kontes (Hari 1) : 4 September 2013</li>
+        <li>Kontes (Hari 2) : 5 September 2013</li>
+    </ul>
+
+    <p>Pertanyaan mengenai OSN Terbuka dapat dikirim melalui email ke <a href="#">gede.wahyu@ui.ac.id</a> dengan subjek email mengandung tag [OPEN OSN].</p>
+    <p>Demikian pengumuman ini disampaikan. Selamat berkompetisi dan jadilah yang terbaik!</p>
+    <p>
+        <em>Sukses TOKI !</em><br />
+        <em>Go Get Golds !</em><br />
+        <em>Depok, 25 Agustus 2013</em>
+    </p>
+    <p>
+        <em>Tim Olimpiade Komputer Indonesia</em><br />
+        <em>Koordinator OSN Informatika Terbuka 2013</em><br />
+        <em>Gede Wahyu Adi Pramana</em>
+    </p>
+</div>
